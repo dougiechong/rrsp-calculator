4c99221e219f1adeb5aec8f42f3fe464de9bed2c
dougiechong <dougiechong@gmail.com>
2020-01-13 22:26:13 -0500

    add some default values

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,11 +7,19 @@ import { FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-    formControl = new FormControl('');
-    bioSection = new FormGroup({
-        firstName: new FormControl(''),
-        lastName: new FormControl(''),
-        age: new FormControl('')
-      });
+  formControl = new FormControl('');
+  bioSection = new FormGroup({
+    income: new FormControl(100000),
+    contribution: new FormControl(10000),
+    gains: new FormControl(4),
+    ageStarted: new FormControl(''),
+    retirementBracket: new FormControl(''),
+    workingTaxBracket: new FormControl(''),
+    avgRealizedGains: new FormControl(''),
+    ageRetirement: new FormControl(''),
+    retirementWithdrawal: new FormControl(''),
+    rrspBonus: new FormControl('')
+  });
 }
+
 
